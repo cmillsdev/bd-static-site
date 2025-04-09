495314eefc937def0c1252554aad3cb026e121ee
Cody <vzualbandt@gmail.com>
2025-04-09 16:45:48 -0700

    I can only be mad at myself

--- src/generate_page.py
+++ src/generate_page.py
@@ -6,7 +6,7 @@ def open_file(filepath):
         md = f.read()
     return md
 
-def generate_pages_recursive(file_list, source, dest):
+def generate_pages_recursive(file_list, source, dest, basepath):
     if not len(file_list):
         return True
     file_dest = file_list[0].replace(source,dest).replace(".md", ".html")
@@ -15,8 +15,8 @@ def generate_pages_recursive(file_list, source, dest):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     
-    generate_page(file_list[0], "template.html", file_dest)
-    generate_pages_recursive(file_list[1:], source, dest)
+    generate_page(file_list[0], "template.html", file_dest, basepath)
+    generate_pages_recursive(file_list[1:], source, dest, basepath)
 
 def extract_title(markdown):
     blocks = markdown_to_blocks(markdown)
@@ -24,7 +24,7 @@ def extract_title(markdown):
         if (block_to_block_type(b)) == BlockType.HEADING:
             return b.strip("#").strip()
 
-def generate_page(source_path, template_path, dest_path):
+def generate_page(source_path, template_path, dest_path, basepath):
     print(f"Generating page from {source_path} to {dest_path} using {template_path}")
     source_md = open_file(source_path)
     template_html = open_file(template_path)
@@ -34,8 +34,8 @@ def generate_page(source_path, template_path, dest_path):
 
     template_html = template_html.replace("{{ Title }}", title)
     template_html = template_html.replace("{{ Content }}", conv_html.to_html())
-    template_html = template_html.replace('href="/', f'href="docs')
-    template_html = template_html.replace('src="/', f'src="docs')
+    template_html = template_html.replace('href="/', f'href="{basepath}')
+    template_html = template_html.replace('src="/', f'src="{basepath}')
 
     with open(dest_path, "w") as f:
         f.write(template_html)
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from textnode import TextNode, TextType
 from copystatic import get_file_list, copy_files
 from generate_page import generate_pages_recursive
 
-def overwrite_dir(source="static", dest="public"):
+def overwrite_dir(source="static", dest="docs"):
     if os.path.isdir(dest):
         rmtree(dest)
     os.makedirs(dest)
@@ -14,11 +14,11 @@ def overwrite_dir(source="static", dest="public"):
 
 def main():
     if len(sys.argv) > 1:
-        base_dir = sys.argv[1]
+        basepath = sys.argv[1]
     else:
-        base_dir = ""
-    overwrite_dir(dest=f"{base_dir}docs")
-    generate_pages_recursive(get_file_list("content"), "content", f"{base_dir}docs")
+        basepath = "/"
+    overwrite_dir()
+    generate_pages_recursive(get_file_list("content"), "content", "docs", basepath)
 
 if __name__ == "__main__":
     main()
